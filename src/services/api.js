b9542a423d1b54441056ab5a8f4bff8c125eb5fb
marcmjn <marcmjn@yahoo.com>
2025-02-01 19:14:23 -0700

    done

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "d395494670ab9573f3168b4a08ab49ed"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+    const data = await response.json()
+    return data.results
+}
